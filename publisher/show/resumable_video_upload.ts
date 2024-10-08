d32f7155efe61a75a52dae505bec9ed49c68d09a
Yuxuan Xie <teststaybaka@gmail.com>
2024-10-08 10:14:40 -0700

    Renamed uri to url. Removed crc32c validation. Release

--- publisher/show/resumable_video_upload.ts
+++ publisher/show/resumable_video_upload.ts
@@ -1,24 +1,19 @@
 import { PrimitiveType, MessageDescriptor } from '@selfage/message/descriptor';
 
 export interface ResumableVideoUpload {
-  uri?: string,
-  crc32c?: string,
+  url?: string,
   byteOffset?: number,
 }
 
 export let RESUMABLE_VIDEO_UPLOAD: MessageDescriptor<ResumableVideoUpload> = {
   name: 'ResumableVideoUpload',
   fields: [{
-    name: 'uri',
+    name: 'url',
     index: 1,
     primitiveType: PrimitiveType.STRING,
   }, {
-    name: 'crc32c',
-    index: 2,
-    primitiveType: PrimitiveType.STRING,
-  }, {
     name: 'byteOffset',
-    index: 3,
+    index: 2,
     primitiveType: PrimitiveType.NUMBER,
   }],
 };
